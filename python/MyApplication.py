611c70570cb730910dff375696f0b911b625fc2d
ljnath <lakhya.sci@gmail.com>
2023-09-28 20:12:49 +0530

    Updated example python program as per Gatekeeper v3.3

--- python/MyApplication.py
+++ python/MyApplication.py
@@ -14,18 +14,18 @@ class Hardware:
         mac_address = uuid.UUID(int=uuid.getnode()).hex[-12:]
         key = sha256(mac_address.encode('utf-8')).hexdigest()
         return key.upper()
-    
+
 
 class MyApplication:
     """
     Class to generate prime numbers
     """
 
-    def __init__(self, api_key) -> None:
-        self.__api_key = api_key
-        
+    def __init__(self, app_token) -> None:
+        self.__app_token = app_token
+
         hardware = Hardware()
-        self.__client_id = hardware.get_unique_id()
+        self.__node_id = hardware.get_unique_id()
         self.__application_id = "PY_1d43e73354ba46a6813e52ab47d968f4"
         self.__application_name = "PythonTestApplication"
         self.__application_version = "1.0.0"
@@ -36,42 +36,45 @@ class MyApplication:
         """
         while True:
             try:
+
+                print('\nWaiting for 3 seconds before checking for license...')
+                sleep(3)
+
                 url = 'https://app.ljnath.com/gatekeeper/v3/licenses/license'
                 data = {
                     "application_id": self.__application_id,
                     "application_name": self.__application_name,
                     "application_description": "This is a gatekeeper example application written in python",
                     "application_version": self.__application_version,
-                    "client_id": self.__client_id,
-                    "client_description": "client with ID "+self.__client_id
+                    "node_id": self.__node_id,
+                    "node_description": f"Node with ID {self.__node_id}"
                 }
 
                 headers = {
                     'Content-Type': 'application/json',
                     'Accept': 'application/json',
-                    'x-api-key': self.__api_key
+                    'x-app-token': self.__app_token
                 }
 
                 response = requests.post(url, json=data, headers=headers)
+                json_response = json.loads(response.text)
                 if response.status_code == 200:
-                    json_response = json.loads(response.text)
                     state = json_response['state']
                     if state == 'ACTIVE':
                         print("License state is active; all good !")
                     elif state == 'EXPIRED':
-                        print("License has expired. Expiry mesage: " + json_response['expiry_message'])
+                        print(f"License has expired. Expiry mesage: {json_response['expiry_message']}")
                     elif state == 'BLOCKED':
                         print("License is blocked")
                     elif state == 'IGNORED':
                         print("License state is ignored")
-                        
+                else:
+                    print(f'Unexpected response code: {response.status_code}. Message: {json_response["message"]}')
+
             except HTTPError:
                 print('Failed to check license status')
 
-            print('\nRe-Checking license state in 3 seconds ...')
-            sleep(3)
 
-            
-if __name__ == '__main__':    
-    my_application = MyApplication('your-gatekeeper-api-key')
+if __name__ == '__main__':
+    my_application = MyApplication('app-token-created-for-this-application')
     my_application.check_license()
